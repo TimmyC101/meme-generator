3a62da7b37e46c4982ea337ad8aacf945db4ce1e
TimmyC101 <tcalhoun101@yahoo.com>
2020-01-09 15:57:44 -0600

    Updates

---
+++ precache-manifest.366a2f46e42e8b80ef50d1264a301009.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5473c1e4eb98387e04b15de3f6a87be",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "2b676cd0fb6aedda5ab4",
+    "url": "/meme-generator/static/css/main.cecb1ab7.chunk.css"
+  },
+  {
+    "revision": "17b5313b85fc0d571626",
+    "url": "/meme-generator/static/js/2.485f1c84.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/meme-generator/static/js/2.485f1c84.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2b676cd0fb6aedda5ab4",
+    "url": "/meme-generator/static/js/main.b2aeecc0.chunk.js"
+  },
+  {
+    "revision": "b8e1865f2d9a05726581",
+    "url": "/meme-generator/static/js/runtime-main.57e0be95.js"
+  }
+]);
